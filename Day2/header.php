be5823345d1590db376939dfb95953c5308eacd5
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-11-07 16:05:32 +1300

    Create header.php

---
+++ Day2/header.php
@@ -0,0 +1,23 @@
+<?php
+    include "./function/user-functions.php";
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+        <header>
+            <nav>
+                <li><a href="">nav1</a></li>
+                <li><a href="">nav2</a></li>
+                <li><a href="">nav3</a></li>
+                <li><a href="">nav4</a></li>
+                <li>contact us</li>
+                
+            </nav>
+</header>
+	</body>
+</html>
