282333248a1dda97b802c07b577685c2498e312c
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-11-07 16:04:31 +1300

    Create Index.php

---
+++ Day2/Index.php
@@ -0,0 +1,23 @@
+<?php
+    include 'header.php';
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+        
+        
+        <section>
+            <div class="someMainContent">
+                <h1>Hi there!</h1>
+            </div>
+        </section>
+
+	</body>
+</html>
+
+
