3f7f31ea05093ccb25e8d9aa77ead869ba91f7f0
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-11-07 16:33:55 +1300

    Create index.php

---
+++ Day4/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      /*
+      $GLOBALS
+      $_POST
+      $_GET
+      $_COOKIES
+      $_SESSION
+      /*
+    ?>
+    
+    <form method="GET">
+      <input type="hidden" name="name" value="Dini">
+      <button type="submit">PRESS ME</button>
+    </form> 
+    
+    <?php
+      echo $_GET['name'];
+    ?>
+   </body>
+</html> 
