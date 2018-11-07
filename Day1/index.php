36c6e7f6bc314bc030023ce8f1e55bed6ba6b2e0
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-11-07 15:34:28 +1300

    Create index.php

---
+++ Day1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+        
+        <?php
+            $array = array("Daniel", "Jane", "Jacob","John","Mariane");
+            
+            echo $array[]0;
+        
+            /*
+            foreach ($array as $newarray ){
+                echo "My name is ".$newarray."<br>";
+            }
+            */
+        ?>
+
+	</body>
+</html>
