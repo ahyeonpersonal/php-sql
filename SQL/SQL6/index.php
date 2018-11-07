fc78479f43584615c7962cd27c1e34320bf7dd07
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-11-08 12:03:17 +1300

    Create index.php

---
+++ SQL/SQL6/index.php
@@ -0,0 +1,14 @@
+<?php
+  include_once 'includes/dbh.inc.php';
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+        $conn; <!-- the way to connect database-->
+	</body>
+</html>
