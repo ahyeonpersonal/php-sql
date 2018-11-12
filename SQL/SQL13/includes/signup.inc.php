3378b8ea6308d9913eaec31da39aa64d74a747f3
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-11-13 11:44:22 +1300

    Displaying Error while keeping the correct data

--- SQL/SQL13/includes/signup.inc.php
+++ SQL/SQL13/includes/signup.inc.php
@@ -1,45 +1,38 @@
 <?php
+        if(isset($_POST['submit'])){
+                include_once 'dbh.inc.php';
 
-    //check if signup form is submitted or not
-    if (isset($_POST['submit'])){ //'submit' : name of the button
-        
-        //database connection
-        include_once 'dbh.inc.php';
-        
-        $first = $_POST['first'];
-        $last = $_POST['last'];
-        $email = $_POST['email'];
-        $uid = $_POST['uid'];
-        $pwd = $_POST['pwd'];
+                $first=mysqli_real_escape_string($conn, $_POST['first']);
+                $last=mysqli_real_escape_string($conn,$_POST['last']);
+                $email=mysqli_real_escape_string($conn,$_POST['email']);
+                $uid=mysqli_real_escape_string($conn,$_POST['uid']);
+                $pwd=mysqli_real_escape_string($conn,$_POST['pwd']);
 
-        //error msg : when user miss one of the form
-        //always put the error first, and then think about the success step
-        //otherwise, php would proceed next step regardless of error
-        if( empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd) ){
-            header("Location: ../index.php?signup=empty");
-            exit();
-        }else{
-            //check if input characters are valid
-            if(!preg_match("/^[a-zA-Z]*$/, $first")||!preg_match("/^[a-zA-Z]*$last")){
-                header("Location : ../index.php?signup=char");
-                exit();
-            }else{
-                //check if email is valid
-                if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-                header("Location: ../index.php?signup=email&first=$first&last=$last&uid=$uid");
-                //might be only email is wrong =? means that first, last, uid might be correct
-                // &first=$first&last=$last&uid=$uid
-                exit();
+                 //always put the error first, and then think about the success step
+                //otherwise, php would proceed next step regardless of error
+                if( empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd) ){
+                        header("Location: ../index.php?signup=empty");
+                        exit();
                 }else{
-                header("Location: ../index.php?signup=success");
-                exit();
+                        if(!preg_match("/^[a-zA-Z ]*$/",$first) || !preg_match("/^[a-zA-Z ]*$/",$last)){
+                                //Inlvaid first name & last name type : 이름이 옳지 않은 경우 
+                                header("Location : ../index.php?signup=char ");
+                                exit();
+                        }else{
+                                if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                                        //Valid name but invalid email : 이름은 옳고 이메일은 옳지 않은 경우
+                                        header("Location:../index.php?singup=email&first=$fisrt$last=$last&uid=$uid");
+                                        exit();
+                                }else{
+                                        $sql = "INSERT INTO users(user_first, user_last, user_email, user_uid, user_pwd)
+                                        VALUES ('$first','$last','$email','$uid','$pwd');";
+                                        mysqli_query($conn, $sql);
+                                        header("Location: ../index.php?signup=success");
+                                        exit();
+                                }
+                        }
                 }
-            
-            }
-        }
-    }else{
-        //echo "You did not fill out a form!";
-        
-        //back to index.php page
-        header("Location: ../index.php?signup=error");//add something extra behind the URL
-    }
+        }else{
+                header("Location:../index.php?signup=error");
+        }        
+?>
