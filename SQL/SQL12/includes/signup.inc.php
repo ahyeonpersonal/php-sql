58b43e00ff9bb56b890cc7ac0deb13f30f185838
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-11-13 10:49:53 +1300

    Error handlers in PHP

--- SQL/SQL12/includes/signup.inc.php
+++ SQL/SQL12/includes/signup.inc.php
@@ -1,32 +1,34 @@
 <?php
 
-    //check if signup form is submitted or not
-    if (isset($_POST['submit'])){ //'submit' : name of the button
-        
-        include_once 'dbh.inc.php';
-        
-        $first = $_POST['first'];
-        $last = $_POST['last'];
-        $email = $_POST['email'];
-        $uid = $_POST['uid'];
-        $pwd = $_POST['pwd'];
+        if(isset($_POST['submit'])){
+                include_once 'dbh.inc.php';
 
-        //error msg : when user miss one of the form
-        //always put the error first, and then think about the success step
-        //otherwise, php would proceed next step regardless of error
-        if( empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd) ){
-            header("Location: ../index.php?signup=empty");
-        }else{
-            if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
-                header("Location: ../index.php?signup=invalidemail");
-            }else{
-                echo "Sign up the user!";
-            }
-        }
-    
-     }else{
-        //echo "You did not fill out a form!";
+                $first=mysqli_real_escape_string($conn, $_POST['first']);
+                $last=mysqli_real_escape_string($conn,$_POST['last']);
+                $email=mysqli_real_escape_string($conn,$_POST['email']);
+                $uid=mysqli_real_escape_string($conn,$_POST['uid']);
+                $pwd=mysqli_real_escape_string($conn,$_POST['pwd']);
+
+                 //always put the error first, and then think about the success step
+                //otherwise, php would proceed next step regardless of error
+                if( empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd) ){
+                        header("Location: ../index.php?signup=empty");
+                }else{
+                        if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+                        header("Location: ../index.php?signup=invalidemail");
+                        }else{
+                        echo "Your've been signed up :->";
+                        }
+                }
+                
+                
         
-        //back to index.php page
-        header("Location: ../index.php?signup=error");//add something extra behind the URL
-    }
+                $sql = "INSERT INTO users(user_first, user_last, user_email, user_uid, user_pwd)
+                        VALUES ('$first','$last','$email','$uid','$pwd');";
+                mysqli_query($conn, $sql);
+
+                header("Location: ../index.php?signup=success");
+        }else{
+                header("Location:../index.php?signup=error");
+        }        
+?>
