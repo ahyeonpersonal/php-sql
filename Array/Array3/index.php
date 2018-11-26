110130c38ad36905b45e4f3bb065617f9bb97619
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-11-27 11:00:00 +1300

    different types of array

---
+++ Array/Array3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+  	<?php
+      //Indexed arrays
+      $data = array("Dini","Na", "Hello");
+      echo $data[0]; //result : Dini
+      
+      //Associative arrays
+      $data = array("first"=>"Dini", "last"=>"Na", "age"=>25);
+      
+      //Multidimensional arrays
+      $data = array(array("Dini","Na"), "Auckland", "NewZealand");
+    ?>  
+  </body>
+</html>
