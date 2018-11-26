6a041cdfd50a591eed76872730fd60786ba3e48d
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-11-27 11:11:52 +1300

    Associative Array

---
+++ Array/Array4/index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+        <?php
+            
+            //Associative arrays
+            /*
+                $data = array(
+                "first"=>"Dini", 
+                "last"=>"Na", 
+                "age"=>25
+            );
+            echo $data['first'];
+            */
+        
+            $data["first"] = "Dini";
+            $data["last"] = "Na";
+            $data["age"] = 25;
+            print_r($data);
+            
+            
+        ?>
+	</body>
+</html>
