0a2005e342da5768d9b788fb566f64d127823760
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-12-03 18:08:34 +1300

    Create index.php

---
+++ 54/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+        <form action='deletefile.php' method='POST'>
+            <button type="submit" name="submit">Delete File </button>
+        </form>
+	</body>
+</html>
