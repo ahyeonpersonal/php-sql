106cedc04bbde0b369db12848b6b67f8627dc618
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-12-05 10:43:15 +1300

    Create index.php

---
+++ 56/index.php
@@ -0,0 +1,16 @@
+<!--
+    in case we don't know file type(extension)
+-->
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+	<body>
+        <form action="deletefile.php" method="post">
+            <input type="text" name="filename" placeholder="Seperate each name with a comma(,)" style="with:800px;" />
+            <button type='submit' name='submit'>Delete file</button>
+        </form>
+	</body>
+</html>
