95a5004e3d1d5876a97e783679ce63144bba84a6
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-12-05 16:14:24 +1300

    Create header.php

---
+++ 57/header.php
@@ -0,0 +1,10 @@
+<?php
+	include 'dbh.php';
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
